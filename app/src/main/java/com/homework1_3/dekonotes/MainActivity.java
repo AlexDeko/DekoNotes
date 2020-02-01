693663f4d37256cf1d4f8bcd08993e2a22dbb0cb
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-02-02 01:22:43 +0300

    FabAndFrame

--- app/src/main/java/com/homework1_3/dekonotes/MainActivity.java
+++ app/src/main/java/com/homework1_3/dekonotes/MainActivity.java
@@ -1,15 +1,12 @@
 package com.homework1_3.dekonotes;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
 
     @Override
---
+++ app/src/main/java/com/homework1_3/dekonotes/NoteSheet.java
@@ -0,0 +1,9 @@
+package com.homework1_3.dekonotes;
+
+import android.icu.util.Calendar;
+
+public class NoteSheet {
+    private String title;
+    private String text;
+    private Calendar calendar;
+}
