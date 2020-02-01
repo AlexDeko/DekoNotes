0ed64be686ea1f3fc8afe9f45ad44b8f0e562760
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-02-01 13:22:11 +0300

    Initial commit

---
+++ app/src/main/java/com/homework1_3/dekonotes/MainActivity.java
@@ -0,0 +1,28 @@
+package com.homework1_3.dekonotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+       // myToolbar.setNavigationIcon(R.drawable.ic_delete3d);
+        myToolbar.setNavigationIcon(getResources()
+                .getDrawable(R.drawable.ic_notepad_12, null));
+
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
