9f753ab72e6857f120e6296b267f7106c9d596a0
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-02-02 14:09:24 +0300

    Swipe, Toolbar, ListView, SharedPreferences

--- app/src/main/java/com/homework1_3/dekonotes/MainActivity.java
+++ app/src/main/java/com/homework1_3/dekonotes/MainActivity.java
@@ -1,15 +1,44 @@
 package com.homework1_3.dekonotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final static String LOG_TAG = "sample";
+    private final static String TITLE = "title";
+    private final static String SUBTITLE = "subtitle";
+    private final static String TEXT = "text";
+    private final static String PREF = "pref";
+    List<Map<String, String>> simpleAdapterContent;
+    private ListView list;
+    private SharedPreferences sharedPref;
+    private String result;
+    private String[] content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +47,84 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         myToolbar.setLogo(getDrawable(R.drawable.ic_notepad_12));
+
+        updateList();
+        content = prepareContent();
+        final BaseAdapter listContentAdapter = createAdapter(content);
+        list.setAdapter(listContentAdapter);
+        listContentAdapter.notifyDataSetChanged();
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
+                //final View item = (TextView) parent.getItemAtPosition(position);
+                view.animate().setDuration(20).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                // list.removeView(item);
+                                simpleAdapterContent.remove(position);
+                                listContentAdapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+                //  list.removeView(item);
+
+                // listContentAdapter.notifyDataSetChanged();
+            }
+        });
+
+        final SwipeRefreshLayout swipeLayout = findViewById(R.id.swiperefresh);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            // Будет вызван, когда пользователь потянет список вниз
+            @Override
+            public void onRefresh() {
+                updateList();
+                swipeLayout.setRefreshing(false);
+            }
+        });
+    }
+
+    private void updateList() {
+        sharedPref = getSharedPreferences(PREF, Context.MODE_PRIVATE);
+        // SharedPreferences.Editor myEditor = sharedPref.edit();
+        if (sharedPref.contains(TEXT)) {
+            result = sharedPref.getString(TEXT, null);
+
+        } else {
+            String largeText = getString(R.string.large_text);
+
+            sharedPref.edit()
+                    .putString(TEXT, largeText)
+                    .apply();
+            result = largeText;
+        }
+        content = prepareContent();
+    }
+
+    @NonNull
+    private BaseAdapter createAdapter(String[] values) {
+        list = findViewById(R.id.list);
+        simpleAdapterContent = new ArrayList<>();
+
+        for (String value : values) {
+            Map<String, String> row = new HashMap<>();
+            row.put(TITLE, value);
+            row.put(SUBTITLE, String.valueOf(value.length()));
+            simpleAdapterContent.add(row);
+        }
+        return new SimpleAdapter(
+                this,
+                simpleAdapterContent,
+                R.layout.list_item,
+                new String[]{TITLE, SUBTITLE},
+                new int[]{R.id.textItem1, R.id.textItem2}
+        );
+    }
+
+    @NonNull
+    private String[] prepareContent() {
+        return result.split("\n\n");
     }
 
     @Override
@@ -25,7 +132,6 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
 
 
     @Override
