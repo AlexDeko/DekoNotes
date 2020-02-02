4872bb200daca128f8dcb2d79d797e92a9148dc5
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-02-03 00:32:27 +0300

    Notes ; toDo Calendar

--- app/src/main/java/com/homework1_3/dekonotes/NotesActivity.java
+++ app/src/main/java/com/homework1_3/dekonotes/NotesActivity.java
@@ -1,14 +1,23 @@
 package com.homework1_3.dekonotes;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class NotesActivity extends AppCompatActivity {
 
@@ -16,7 +25,12 @@ public class NotesActivity extends AppCompatActivity {
     private Button btnSaveNote;
     private SharedPreferences myNoteSharedPref;
     private static String NOTE_TEXT = "note_text";
-
+    private ImageView imgBtnCalendar;
+    private CheckBox checkDeadline;
+    private EditText title;
+    private EditText text;
+    private EditText dateCalendar;
+    private DatePickerDialog datePickerDialog;
 
 
     @Override
@@ -28,10 +42,17 @@ public class NotesActivity extends AppCompatActivity {
         myToolbar.setLogo(getDrawable(R.drawable.ic_notepad_12));
 
         initViews();
+        setDateCalendar();
         getDateFromSharedPref();
     }
 
     private void initViews() {
+        imgBtnCalendar = findViewById(R.id.imgBtnCalendar);
+        checkDeadline = findViewById(R.id.checkDeadline);
+        title = findViewById(R.id.editTitle);
+        text = findViewById(R.id.editText);
+        dateCalendar = findViewById(R.id.editDateDeadline);
+
         inputNote = findViewById(R.id.inputNote);
         btnSaveNote = findViewById(R.id.btnSaveNote);
         myNoteSharedPref = getSharedPreferences("MyNote", MODE_PRIVATE);
@@ -49,9 +70,53 @@ public class NotesActivity extends AppCompatActivity {
         });
     }
 
+    private void setDateCalendar(){
+
+        Calendar todayCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(
+                this,
+                onDateSet,
+                todayCalendar.get(Calendar.YEAR),
+                todayCalendar.get(Calendar.MONTH),
+                todayCalendar.get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+
+
+    }
+
+    private void DatePickerDialog onDateSet(){
+        datePickerDialog = ;
+        DatePickerDialog.OnDateSetListener;
+        return null;
+    }
+
     private void getDateFromSharedPref(){
         String noteTxt = myNoteSharedPref.getString(NOTE_TEXT, "");
         inputNote.setText(noteTxt);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_notes, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        Intent targetIntent;
+//        if (id == R.id.action_settings) {
+//            Toast.makeText(Not.this, getString(R.string.toast_settings),
+//                    Toast.LENGTH_LONG).show();
+//            targetIntent = new Intent(MainActivity.this, NotesActivity.class);
+//            startActivity(targetIntent);
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
