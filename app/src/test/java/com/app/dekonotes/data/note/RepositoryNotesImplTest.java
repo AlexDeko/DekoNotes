add28ac088c272b0eddb91d3935a6446a9a950a7
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-02-22 14:59:33 +0300

    RepositoryNotesImplTest get new Tests(upgrade, insert, delete) toDo getAll

--- app/src/test/java/com/app/dekonotes/data/note/RepositoryNotesImplTest.java
+++ app/src/test/java/com/app/dekonotes/data/note/RepositoryNotesImplTest.java
@@ -6,11 +6,18 @@ import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import io.reactivex.Completable;
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
 import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -30,6 +37,8 @@ public class RepositoryNotesImplTest {
     private NoteDao dao = mock(NoteDao.class);
     // В конструктор передали чучело
     private RepositoryNotesImpl repository = new RepositoryNotesImpl(dao);
+
+
 
     // Это вызовется после каждого метода с аннотацией @Test
     @After
@@ -59,37 +68,67 @@ public class RepositoryNotesImplTest {
 
 
     }
+    
+    @Test
+    public void get_all_EXPECT_note_in_repository() {
+        // Моделируем поведение
+        when(dao.getAll())
+                .thenReturn(Observable.just((List<Note>) repository.getAll()));
 
-//    @Test
-//    public void insert_EXPECT_note_in_repository() {
-//        // Моделируем поведение
-//        when(dao.getAll(Observable<List<TestData.TEST_NOTE>>))
-//                .thenReturn(Observabl);
-//
-//        repository.insert(TestData.TEST_NOTE)
-//                // Вызываем метод у Rx для тестов. Можно проверить в каком состоянии Completable
-//                .test()
-//                // Ожидаем добавленее новой заметки. В случае ошибки можно вызвать assertError
-//                .assertValue(TestData.TEST_NOTE);
-//
-//        // Проверяем, что вызвали метод у чучела
-//        verify(dao).getAll();
-//    }
-//
-//    @Test
-//    public void insert_EXPECT_note_in_repository() {
-//        // Моделируем поведение
-//        when(dao.insertNote(TestData.TEST_NOTE))
-//                .thenReturn(Completable.complete());
-//
-//        repository.insert(TestData.TEST_NOTE)
-//                // Вызываем метод у Rx для тестов. Можно проверить в каком состоянии Completable
-//                .test()
-//                // Ожидаем добавленее новой заметки. В случае ошибки можно вызвать assertError
-//                .assertValue(TestData.TEST_NOTE);
-//
-//        // Проверяем, что вызвали метод у чучела
-//        verify(dao).insertNote(TestData.TEST_NOTE);
-//    }
+        repository.getAll()
+                // Вызываем метод у Rx для тестов. Можно проверить в каком состоянии Observable
+                .test()
+
+                // Ожидаем добавленее новой заметки. В случае ошибки можно вызвать assertError
+                .assertValue((List<Note>) repository.getAll());
+
+        // Проверяем, что вызвали метод у чучела
+        verify(dao).getAll();
+    }
+
+    @Test
+    public void insert_EXPECT_note_in_dao() {
+        when(dao.insertNote(TestData.TEST_NOTE))
+                .thenReturn(Completable.complete());
+
+        repository.insert(TestData.TEST_NOTE)
+                // Вызываем метод у Rx для тестов. Можно проверить в каком состоянии Completable
+                .test()
+                // Ожидаем добавленее новой заметки и вызов complete. В случае ошибки можно вызвать assertError
+                .assertComplete();
+
+        // Проверяем, что вызвали метод у чучела
+        verify(dao).insertNote(TestData.TEST_NOTE);
+    }
+
+    @Test
+    public void delete_EXPECT_note_from_dao() {
+        when(dao.delete(TestData.TEST_NOTE))
+                .thenReturn(Completable.complete());
+
+        repository.delete(TestData.TEST_NOTE)
+                // Вызываем метод у Rx для тестов. Можно проверить в каком состоянии Completable
+                .test()
+                // Ожидаем добавленее новой заметки и вызов complete. В случае ошибки можно вызвать assertError
+                .assertComplete();
+
+        // Проверяем, что вызвали метод у чучела
+        verify(dao).delete(TestData.TEST_NOTE);
+    }
+
+    @Test
+    public void update_EXPECT_note_in_dao() {
+        when(dao.update(TestData.TEST_NOTE))
+                .thenReturn(Completable.complete());
+
+        repository.update(TestData.TEST_NOTE)
+                // Вызываем метод у Rx для тестов. Можно проверить в каком состоянии Completable
+                .test()
+                // Ожидаем добавленее новой заметки и вызов complete. В случае ошибки можно вызвать assertError
+                .assertComplete();
+
+        // Проверяем, что вызвали метод у чучела
+        verify(dao).update(TestData.TEST_NOTE);
+    }
 
 }
